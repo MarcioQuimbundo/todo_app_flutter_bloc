c6234427544eae4525fad1fb733487c57e98c598
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-07-06 14:46:58 +0100

    TodoItem added

--- lib/src/ui/widgets/filtered_todos.dart
+++ lib/src/ui/widgets/filtered_todos.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app_flutter/src/ui/widgets/loading_indicator.dart';
+import 'package:todo_app_flutter/src/ui/widgets/todo_item.dart';
 import 'package:todos_app_core/todos_app_core.dart';
 import 'package:todo_app_flutter/src/blocs/blocs.dart';
 //import 'package:todo_app_flutter/src/ui/widgets.dart';
