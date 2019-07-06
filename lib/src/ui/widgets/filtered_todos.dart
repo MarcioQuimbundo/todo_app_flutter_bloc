698e4cfd5ac6d6dddfd1ddf724ba9c1a324346d1
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-07-06 14:44:47 +0100

    loading_indicator added

--- lib/src/ui/widgets/filtered_todos.dart
+++ lib/src/ui/widgets/filtered_todos.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app_flutter/src/ui/widgets/loading_indicator.dart';
 import 'package:todos_app_core/todos_app_core.dart';
 import 'package:todo_app_flutter/src/blocs/blocs.dart';
 //import 'package:todo_app_flutter/src/ui/widgets.dart';
