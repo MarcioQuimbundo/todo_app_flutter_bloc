004ab2c725956a7c2856a0386416cfa9d40dcf47
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-07-06 14:51:53 +0100

    stats widget added

--- lib/src/ui/widgets/filtered_todos.dart
+++ lib/src/ui/widgets/filtered_todos.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app_flutter/src/ui/widgets/delete_todo_snack_bar.dart';
 import 'package:todo_app_flutter/src/ui/widgets/loading_indicator.dart';
 import 'package:todo_app_flutter/src/ui/widgets/todo_item.dart';
 import 'package:todos_app_core/todos_app_core.dart';
